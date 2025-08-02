5bb7076c84730803fdf56f4fe05164cfe2935896
Ayush <ayushvishhhhh@gmail.com>
2025-08-02 21:01:48 +0530

    ended the final

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const box = document.getElementById('searchDiv')
 const btn = document.getElementById('searchIcon')
 const input = document.getElementById('input')
-
 btn.addEventListener('click' , () => {
+    input.classList.remove('locked')
     box.classList.toggle('active')
     btn.classList.toggle('active')
+    input.classList.toggle('active')
+    
     document.body.classList.toggle('darkmode')
 
     if(box.classList.contains('active')) {
